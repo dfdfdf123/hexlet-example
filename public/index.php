11c4a12dfe78b4a4217c7c5ec94ecc66278c40a4
johnromanoff <johnromanoff@yandex.ru>
2019-01-16 22:06:25 +0300

    add templates

--- public/index.php
+++ public/index.php
@@ -10,8 +10,17 @@ $configuration = [
 
 $app = new \Slim\App($configuration);
 
+$container = $app->getContainer();
+
+$container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+
 $app->get('/', function ($request, $respones) {
     return $respones->write('Welcome to Slim');
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id']];
+    return $this->renderer->render($response, 'users/show.phtml', $params);
 });
 
 $app->run();
